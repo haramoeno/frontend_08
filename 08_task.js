71f0e429259e6adf03f0dc8cf5a64b8278e1999e
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-05 10:54:10 +0000

    課題提出

---
+++ 08_task.js
@@ -0,0 +1,13 @@
+function triangleArea(p1, p2) {
+    let p = p1 * p2 /2;
+    return p;
+}
+
+console.log(triangleArea(6,2));
+
+var triangleArea = function(p1, p2) {
+    let p = p1 * p2 /2;
+    return p;
+}
+
+console.log(triangleArea(6,2));
